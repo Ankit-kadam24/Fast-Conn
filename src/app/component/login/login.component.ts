f3966f1a3ca087429483a7b84e61a093ddccee2c
Devjeet Roy <devjeetroy.dr@gmail.com>
2021-03-07 22:34:30 +0530

    Some CSS fixed in Register & Login Page

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -9,39 +9,12 @@ import { FormGroup} from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  showModal: boolean = false;
-  registerForm!: FormGroup;;
-  submitted = false;
-  constructor(private formBuilder: FormBuilder) { }
- 
-  
- // show()
-   //{
-    //this.showModal = true; 
-  //}
-  
-  hide()
-  {
-    this.showModal = false;
-  }
+
 
   ngOnInit() {
-    this.registerForm = this.formBuilder.group({
-        number: ['', [Validators.required, Validators.minLength(10)]],
-        password: ['', [Validators.required, Validators.minLength(6)]]
-    });
 }
 
-get f() { return this.registerForm.controls; }
-onSubmit() {
-    this.submitted = true;
-    
-    if (this.registerForm.invalid) {
-        return;
-    }
-    if(this.submitted)
-    {
-      this.showModal = false;
-    }
+public submitForLogin(data:any[]){
+  
 }
 }
--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -7,42 +7,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  showModal: boolean = false;
-  registerForm!: FormGroup;;
-  submitted = false;
-  constructor(private formBuilder: FormBuilder) { }
-  show()
-  {
-    this.showModal = true; 
-  }
 
-  
-  hide()
-  {
-    this.showModal = false;
-  }
 
   ngOnInit() {
-    this.registerForm = this.formBuilder.group({
-        number: ['', [Validators.required, Validators.minLength(10)]],
-        password: ['', [Validators.required, Validators.minLength(6)]],
-        email: ['', [Validators.required, Validators.email]],
-        name: ['',[Validators.required]]
-
-    });
 }
-// convenience getter for easy access to form fields
-get f() { return this.registerForm.controls; }
-onSubmit() {
-    this.submitted = true;
-    // stop here if form is invalid
-    if (this.registerForm.invalid) {
-        return;
-    }
-    if(this.submitted)
-    {
-      this.showModal = false;
-    }
-}
+  public submitForRegis(data:any[]){
+    
+  }
 
 }
